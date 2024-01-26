6ddc5405ab458f2e16b20261149163a1c32e9445
Saúl Camacho <electrohead1200@gmail.com>
2024-01-25 18:24:24 -0600

    Guardar Evento en la DB y Obtener listado de Eventos

--- controllers/events.js
+++ controllers/events.js
@@ -1,20 +1,37 @@
 const { response } = require('express');
+const Evento = require('../models/Evento');
 
-const getEventos = (req, res = response) => {
+const getEventos = async(req, res = response) => {
+
+    const eventos = await Evento.find().populate('user', 'name');
     res.json({
         ok: true,
-        msg: 'getEventos'
+        eventos
         });
 }
 
-const crearEvento = (req, res = response) => {
+const crearEvento = async(req, res = response) => {
 
-    console.log(req.body);
-    
-    res.json({
-        ok: true,
-        msg: 'crearEventos'
+    const evento = new Evento(req.body);
+
+    try {
+        
+        evento.user = req.uid;
+
+        const eventoGuardado = await evento.save();
+
+        res.json({
+            ok: true,
+            evento: eventoGuardado
         });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
 }
 
 const actualizarEvento = (req, res = response) => {
